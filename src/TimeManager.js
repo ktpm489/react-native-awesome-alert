7f4be73fa5973cae4394915af2befc41138a3284
ktpm489 <ktpm489@gmail.com>
2018-03-29 10:50:43 +0700

    Update TimeManager.js

--- src/TimeManager.js
+++ src/TimeManager.js
@@ -1,6 +1,7 @@
 export default class TimeManager {
   static randomeTimeCheck(savedTime, now, invisibleTime) {
     const passdTime = Math.floor((now - savedTime) / (6 * Math.pow(10, 4)))
-    return now - savedTime > invisibleTime * (6 * Math.pow(10, 4)) // minute
+   // return now - savedTime > invisibleTime * (6 * Math.pow(10, 4)) // minute
+     return true
   }
 }
